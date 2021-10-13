5ea0ad278028fbeddc49833e847dfcda683c8f41
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2021-10-13 19:41:06 -0400

    added new animations so that every animation would be triggered by an item within the window

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
   $(document).ready(function(){
-    $("button").hover(function(){
+    $("div.drop1").hover(function(){
       $("div.textdropbanner").animate({
        top: '40px',
+        // height: '50%',
+        // width: '100%',  
+        opacity: 1.0
+      }, "slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("div.drop2").hover(function(){
+      $("div.textdropbanner").animate({
+       top: '200px',
+        // height: '50%',
+        // width: '100%',  
+        opacity: 1.0
+      }, "slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("div.drop3").hover(function(){
+      $("div.textdropbanner").animate({
+       top: '200px',
+        // height: '50%',
+        // width: '100%',  
+        opacity: 1.0
+      }, "slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("div.drop4").hover(function(){
+      $("div.textdropbanner").animate({
+       top: '150px',
+        // height: '50%',
+        // width: '100%',  
+        opacity: 1.0
+      }, "slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("div.drop5").hover(function(){
+      $("div.textdropbanner").animate({
+       top: '150px',
+        // height: '50%',
+        // width: '100%',  
+        opacity: 1.0
+      }, "slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("div.drop6").hover(function(){
+      $("div.textdropbanner").animate({
+       top: '150px',
         // height: '50%',
         // width: '100%',  
         opacity: 1.0
